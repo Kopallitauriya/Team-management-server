a68f665e0c4681e36b0105361a842c218b240d99
Kopal07 <kopallitauriya70@gmail.com>
2023-12-08 20:44:58 +0530

    deleted: .env file

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api', router)
 
+app.get('/', (req, res) => {
+    res.send("Health Check OK!");
+})
 
 // app.listen(8000, (req, res) => {
 //     console.log("Server is Listening...")
